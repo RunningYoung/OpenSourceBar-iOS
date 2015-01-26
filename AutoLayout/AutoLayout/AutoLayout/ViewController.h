8ba6c9ce260911b37ee1934a3029af98d7c16aae
Maciej Oczko <maciek.oczko@polidea.com>
2015-01-26 11:34:15 +0100

    Initial Commit

---
+++ AutoLayout/AutoLayout/AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Maciej Oczko on 26/01/15.
+//  Copyright (c) 2015 Polidea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

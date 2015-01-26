42a97744e45810546fce86139a23c70c568fd3e6
Maciej Oczko <maciek.oczko@polidea.com>
2015-01-26 11:54:53 +0100

    Initial Commit

---
+++ Views Debugging/ViewsDebugging/ViewsDebugging/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ViewsDebugging
+//
+//  Created by Maciej Oczko on 26/01/15.
+//  Copyright (c) 2015 Polidea. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

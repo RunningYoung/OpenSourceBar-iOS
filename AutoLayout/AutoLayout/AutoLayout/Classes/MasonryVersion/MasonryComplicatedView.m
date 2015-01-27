485110c7f92ae8bbca63256e284bf82d3aafcafb
Maciej Oczko <maciek.oczko@polidea.com>
2015-01-27 11:07:48 +0100

    Added Masonry version

--- AutoLayout/AutoLayout/AutoLayout/Classes/MasonryVersion/MasonryComplicatedView.m
+++ AutoLayout/AutoLayout/AutoLayout/Classes/MasonryVersion/MasonryComplicatedView.m
@@ -9,7 +9,28 @@
 @implementation MasonryComplicatedView
 
 - (void)updateConstraints {
-    //TODO updateConstraints
+
+    [self.title mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_top);
+        make.centerX.equalTo(self.mas_centerX);
+    }];
+
+    [self.container mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.title.mas_bottom);
+        make.bottom.equalTo(self.mas_bottom).with.offset(-10);
+        make.left.equalTo(self.mas_left).with.offset(10);
+        make.right.equalTo(self.mas_right).with.offset(-10);
+    }];
+
+    [self.left mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.and.bottom.equalTo(self.container);
+    }];
+
+    [self.right mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.right.and.bottom.equalTo(self.container);
+        make.left.equalTo(self.left.mas_right);
+    }];
+
     [super updateConstraints];
 }
 
--- AutoLayout/AutoLayout/AutoLayout/Classes/MasonryVersion/MasonryLayoutView.m
+++ AutoLayout/AutoLayout/AutoLayout/Classes/MasonryVersion/MasonryLayoutView.m
@@ -9,7 +9,20 @@
 @implementation MasonryLayoutView
 
 - (void)updateConstraints {
-    //TODO updateConstraints
+
+    [self.complicatedView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_top).with.offset(64);
+        make.left.equalTo(self.mas_left).with.offset(10);
+        make.right.equalTo(self.mas_right).with.offset(-10);
+    }];
+
+    [self.guestView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.complicatedView.mas_bottom);
+        make.bottom.equalTo(self.mas_bottom).with.offset(-10);
+        make.left.equalTo(self.mas_left).with.offset(10);
+        make.right.equalTo(self.mas_right).with.offset(-10);
+    }];
+
     [super updateConstraints];
 }
 

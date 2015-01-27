05a71e60b083d0ec18eb7a0984dccb9e38628c6f
Maciej Oczko <maciek.oczko@polidea.com>
2015-01-27 09:13:13 +0100

    Added manual layout subclasses

---
+++ AutoLayout/AutoLayout/AutoLayout/Classes/Manual/ManualComplicatedVIew.m
@@ -0,0 +1,9 @@
+//
+// Created by Maciej Oczko on 27/01/15.
+// Copyright (c) 2015 Polidea. All rights reserved.
+//
+
+#import "ManualComplicatedVIew.h"
+
+@implementation ManualComplicatedVIew
+@end
---
+++ AutoLayout/AutoLayout/AutoLayout/Classes/Manual/ManualLayoutView.m
@@ -0,0 +1,9 @@
+//
+// Created by Maciej Oczko on 27/01/15.
+// Copyright (c) 2015 Polidea. All rights reserved.
+//
+
+#import "ManualLayoutView.h"
+
+@implementation ManualLayoutView
+@end

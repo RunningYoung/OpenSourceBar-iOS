2038ea231522888d5b28aefeb325fd43f8e361e5
Maciej Oczko <maciek.oczko@polidea.com>
2015-01-27 11:24:17 +0100

    Added Pods and Carthage

---
+++ PackageManagers/CocoaPods/CocoaPodsTest/CocoaPodsTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CocoaPodsTest
+//
+//  Created by Maciej Oczko on 27/01/15.
+//  Copyright (c) 2015 Polidea. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

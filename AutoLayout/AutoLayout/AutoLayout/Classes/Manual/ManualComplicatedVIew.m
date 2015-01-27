be4cc399ba50479b06e113bfba94c90d1d148038
Maciej Oczko <maciek.oczko@polidea.com>
2015-01-27 10:27:13 +0100

    Added buggy (warnings!?) manual layout

--- AutoLayout/AutoLayout/AutoLayout/Classes/Manual/ManualComplicatedVIew.m
+++ AutoLayout/AutoLayout/AutoLayout/Classes/Manual/ManualComplicatedVIew.m
@@ -6,4 +6,56 @@
 #import "ManualComplicatedView.h"
 
 @implementation ManualComplicatedView
+
+- (void)updateConstraints {
+
+    NSDictionary *views = @{
+            @"title" : self.title,
+            @"container" : self.container,
+            @"leftView" : self.left,
+            @"rightView" : self.right,
+    };
+    NSDictionary *metrics = @{
+            @"left" : @10,
+            @"bottom" : @10,
+            @"right" : @10,
+    };
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[title][container]-bottom-|"
+                                                                 options:NSLayoutFormatAlignAllCenterX
+                                                                 metrics:metrics
+                                                                   views:views]];
+
+//    [self addConstraint:[NSLayoutConstraint constraintWithItem:self.title
+//                                                     attribute:NSLayoutAttributeCenterX
+//                                                     relatedBy:NSLayoutRelationEqual
+//                                                        toItem:self
+//                                                     attribute:NSLayoutAttributeCenterX
+//                                                    multiplier:1
+//                                                      constant:0]];
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-left-[container]-right-|"
+                                                                 options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                 metrics:metrics
+                                                                   views:views]];
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[leftView][rightView]|"
+                                                                 options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                 metrics:metrics
+                                                                   views:views]];
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[leftView]|"
+                                                                 options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                 metrics:metrics
+                                                                   views:views]];
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[rightView]|"
+                                                                 options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                 metrics:metrics
+                                                                   views:views]];
+
+
+    [super updateConstraints];
+}
+
 @end
--- AutoLayout/AutoLayout/AutoLayout/Classes/Manual/ManualLayoutView.m
+++ AutoLayout/AutoLayout/AutoLayout/Classes/Manual/ManualLayoutView.m
@@ -6,4 +6,36 @@
 #import "ManualLayoutView.h"
 
 @implementation ManualLayoutView
+
+- (void)updateConstraints {
+
+    NSDictionary *views = @{
+            @"guestView" : self.guestView,
+            @"complicatedView" : self.complicatedView,
+    };
+    NSDictionary *metrics = @{
+            @"top" : @64,
+            @"left" : @10,
+            @"bottom" : @10,
+            @"right" : @10,
+    };
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-top-[complicatedView][guestView]-bottom-|"
+                                                                 options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                 metrics:metrics
+                                                                   views:views]];
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-left-[complicatedView]-right-|"
+                                                                 options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                 metrics:metrics
+                                                                   views:views]];
+
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-left-[guestView]-right-|"
+                                                                 options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                 metrics:metrics
+                                                                   views:views]];
+
+    [super updateConstraints];
+}
+
 @end

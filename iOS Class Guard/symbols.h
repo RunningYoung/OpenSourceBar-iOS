cf934becb1bb18881f24ed7d3e03e5c560b9b814
Michał Laskowski <michal.laskowski@polidea.com>
2015-01-29 12:16:22 +0100

    Updated obfuscate_project script Added sample obsuscated headers and symbols

---
+++ iOS Class Guard/symbols.h
@@ -0,0 +1,60 @@
+// Properties
+
+// Protocols
+
+// Classes
+#ifndef EBAppDelegate
+#define EBAppDelegate a0B
+#endif // EBAppDelegate
+#ifndef EBLoginViewController
+#define EBLoginViewController s58
+#endif // EBLoginViewController
+#ifndef EBWelcomeViewController
+#define EBWelcomeViewController i8h
+#endif // EBWelcomeViewController
+
+// Categories
+
+// Methods
+#ifndef writeClientID
+#define writeClientID h9R
+#endif // writeClientID
+#ifndef setWriteClientID
+#define setWriteClientID setH9R
+#endif // setWriteClientID
+#ifndef readClientPassword
+#define readClientPassword d1L
+#endif // readClientPassword
+#ifndef setReadClientPassword
+#define setReadClientPassword setD1L
+#endif // setReadClientPassword
+#ifndef buttonLogin
+#define buttonLogin s48
+#endif // buttonLogin
+#ifndef setButtonLogin
+#define setButtonLogin setS48
+#endif // setButtonLogin
+#ifndef writeClientPassword
+#define writeClientPassword u0U
+#endif // writeClientPassword
+#ifndef setWriteClientPassword
+#define setWriteClientPassword setU0U
+#endif // setWriteClientPassword
+#ifndef readClientID
+#define readClientID h7q
+#endif // readClientID
+#ifndef setReadClientID
+#define setReadClientID setH7q
+#endif // setReadClientID
+
+// I-vars
+#ifndef _loginErrorField
+#define _loginErrorField n0M
+#endif // _loginErrorField
+#ifndef _clientPasswordField
+#define _clientPasswordField q0Z
+#endif // _clientPasswordField
+#ifndef _clientIDField
+#define _clientIDField j0r
+#endif // _clientIDField
+
